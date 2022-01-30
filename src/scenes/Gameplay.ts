9c187db286a05ee04ac2e8618c6599210ace7cdb
nlc-raman <raman@nolimitcity.com>
2022-01-30 13:40:39 +0530

    fps counter added

--- src/scenes/Gameplay.ts
+++ src/scenes/Gameplay.ts
@@ -1,14 +1,27 @@
+import PixiFps from "pixi-fps";
 import { Scene } from "pixi-scenes";
-
+import * as PIXI from "pixi.js";
 import IScene from "./IScene";
 
 export default class Gameplay extends Scene implements IScene {
+
+    private _fpsCounter: PixiFps;
+    protected _fpsLabel: PIXI.Text;
 
     /**
      * function will be called on intro continue click
      */
     public init(): void {
+        this._fpsCounter = new PixiFps();
+        this._fpsLabel = new PIXI.Text("FPS: ", { fill: 0xffffff });
+        this.addChild(this._fpsLabel, this._fpsCounter);
+        this.resize();
     }
 
 
+    public resize(): void {
+        this._fpsCounter.position.x = this._fpsLabel.width;
+        this._fpsLabel.height = this._fpsCounter.height;
+
+    }
 }
